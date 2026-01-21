71ffedeee85386bbc25915499e7db81373f494cb
vaagisha thakur <vaagishathakur@gmail.com>
2026-01-21 17:25:15 +0000

    new examples

--- js_tut/Basics.js
+++ js_tut/Basics.js
@@ -4,3 +4,16 @@ const roll=123;
 console.log("Name:"+name);
 //roll=124; Not allowed
 console.log(roll);
+
+const addId=24066;
+let addPass="123Vaagisha";
+let city="Bengaluru";
+
+console.table([addId,addPass,city]);//results are showed in a tabular manner
+/*
+var also somethoing like let,const in js earlier to declare a variable 
+but prefer not to use it in modern js as it arises issues in block scope and functional scope.
+*/
+
+//we can also declare like
+rollNo=123;//but not prescribed wrong way :)
