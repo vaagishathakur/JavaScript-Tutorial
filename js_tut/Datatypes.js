3671f987fa6098a724bde9c2fa5f400119ae43aa
vaagisha thakur <vaagishathakur@gmail.com>
2026-01-22 20:04:18 +0000

    Datatypes explained

---
+++ js_tut/Datatypes.js
@@ -0,0 +1,16 @@
+//Datatypes;
+//alert("hii"); this is not the right syntax it can run on web browser only...it gives i pop up showing Hii
+"use strict";//using new version syntax of js
+
+//number :range 2 to the power of 53
+//bigint : used for very large numbers (in some app)
+//String=''or " "
+//boolean
+//NULL:standalone value
+//undefined:means values have not been defined
+//symbol: to find unique
+//object
+
+let name=null;
+console.log(typeof(name));//gives object ..which is an error in js
+console.log(typeof(undefined));//gives undefined
