13686174e39089ba964ace83fd73a38951d3831c
vaagisha thakur <vaagishathakur@gmail.com>
2026-01-27 17:52:35 +0000

    Summary

---
+++ js_tut/DatatypesSummary.js
@@ -0,0 +1,34 @@
+// Primitive DataTypes
+
+// 7 types:
+// String, Number, Boolean, Null, Symbol, undefined, BigInt
+// (Note: JavaScript is Dynamically Typed)
+
+const id = Symbol("123");
+
+/*******************************************************/
+
+// Non-primitive / Reference Data Types
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "SpiderMAN", "CaptainAmerica"]; // Array
+
+// Object
+let myObj = {
+    name: "Vaagisha",
+    course: "Btech",
+    cgpa: 10
+};
+
+// Function in JS can be treated as a variable
+const myFunction = function () {
+    console.log("Hello JavaScript");
+};
+
+// Checking types
+console.log(typeof id);              // symbol
+console.log(typeof heroes);         // object
+console.log(typeof myObj);          // object
+console.log(typeof myFunction);     // function
+console.log(typeof (BigInt));       //function
+console.log(typeof (BigInt(10)));    //BigInt
